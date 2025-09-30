1168be8e966729f51fadd0dd259ff7ad88e5bbbe
Your Name <alisapodgaynaya023@gmail.com>
2025-09-29 21:51:14 -0600

    add js files, start js, update structure, inst vite

---
+++ src/js/scale-rcp.js
@@ -0,0 +1,16 @@
+const baseQty = document.querySelector("#base");
+const desiredQty = document.querySelector("#desired");
+
+const ingredientInput = document.querySelector(".ingredient-group");
+const addIngredient = document.querySelector(".add-new-btn");
+const scaleRecipe = document.querySelector(".result-btn");
+
+console.log(baseQty);
+
+console.log(desiredQty);
+
+console.log(ingredientInput);
+
+console.log(addIngredient);
+
+console.log(scaleRecipe);
