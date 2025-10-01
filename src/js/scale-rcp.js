7c7a62f2ba44d9e7234fd16d88b10a3526b49e5e
Your Name <alisapodgaynaya023@gmail.com>
2025-09-30 21:51:19 -0600

    add js in 1 screen for buttons, inputs

--- src/js/scale-rcp.js
+++ src/js/scale-rcp.js
@@ -2,15 +2,71 @@ const baseQty = document.querySelector("#base");
 const desiredQty = document.querySelector("#desired");
 
 const ingredientInput = document.querySelector(".ingredient-group");
+const inputContainer = document.querySelector(".ingredient-input-wrapper");
+
 const addIngredient = document.querySelector(".add-new-btn");
 const scaleRecipe = document.querySelector(".result-btn");
+const clearAll = document.querySelector(".clear-btn");
 
-console.log(baseQty);
+// =====ADD NEW INGREDIENT LINE======
 
-console.log(desiredQty);
+addIngredient.addEventListener("click", addNewIngredient);
 
-console.log(ingredientInput);
+function addNewIngredient(event) {
+  event.preventDefault();
 
-console.log(addIngredient);
+  // =====clone inputs======
+  let newIngredientInput = ingredientInput.cloneNode(true);
 
-console.log(scaleRecipe);
+  // =====reset inputs======
+  const inputs = newIngredientInput.querySelectorAll("input, select");
+
+  inputs.forEach((input) => {
+    if (input.tagName === "INPUT") {
+      input.value = "";
+    } else if (input.tagName === "SELECT") {
+      input.selectedIndex = 0;
+    }
+  });
+
+  // =====delete button======
+  const inputContainer = newIngredientInput.querySelector(
+    ".ingredient-input-wrapper"
+  );
+
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("del-btn", "button");
+
+  const delIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  delIcon.classList.add("icon");
+  const delIconLink = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "use"
+  );
+
+  delIconLink.setAttribute("href", "../images/icons.svg#icon-cross");
+
+  delIcon.append(delIconLink);
+  deleteButton.append(delIcon);
+
+  // =====add to container======
+
+  inputContainer.insertAdjacentElement("beforeend", deleteButton);
+
+  addIngredient.insertAdjacentElement("beforebegin", newIngredientInput);
+}
+
+// =====clear all inputs======
+
+clearAll.addEventListener("click", clearAllInputs);
+function clearAllInputs() {
+  const inputs = document.querySelectorAll("input, select");
+
+  inputs.forEach((input) => {
+    if (input.tagName === "INPUT") {
+      input.value = "";
+    } else if (input.tagName === "SELECT") {
+      input.selectedIndex = 0;
+    }
+  });
+}
