bfcc95d3cebdfc8e9403279df7bcf6960963f85e
Your Name <alisapodgaynaya023@gmail.com>
2025-10-01 21:38:44 -0600

    add inputs validation, ingr #, update styles

--- src/js/scale-rcp.js
+++ src/js/scale-rcp.js
@@ -9,6 +9,14 @@ const scaleRecipe = document.querySelector(".result-btn");
 const clearAll = document.querySelector(".clear-btn");
 const delButton = document.querySelector(".del-btn");
 
+const numInputs = document.querySelectorAll('input[type="number"]');
+const textInput = document.querySelector('input[type="text"]');
+
+numInputs.forEach((input) =>
+  input.addEventListener("keydown", validateNumInputs)
+);
+textInput.addEventListener("input", validateTextInput);
+
 updateDeleteButtons();
 
 // =====ADD NEW INGREDIENT LINE======
@@ -32,6 +40,14 @@ function addNewIngredient(event) {
       input.selectedIndex = 0;
     }
   });
+  // =====validate inputs======
+  const numInputs = newIngredientInput.querySelectorAll('input[type="number"]');
+  const textInput = newIngredientInput.querySelector('input[type="text"]');
+
+  numInputs.forEach((input) =>
+    input.addEventListener("keydown", validateNumInputs)
+  );
+  textInput.addEventListener("input", validateTextInput);
 
   // =====delete button======
   const deleteButton = newIngredientInput.querySelector(".del-btn");
@@ -40,6 +56,7 @@ function addNewIngredient(event) {
   addIngredient.insertAdjacentElement("beforebegin", newIngredientInput);
 
   updateDeleteButtons();
+  updateIngredientLabels();
 }
 
 // =====clear all inputs======
@@ -49,19 +66,20 @@ function clearAllInputs() {
   const inputs = document.querySelectorAll("input, select");
 
   inputs.forEach((input) => {
-    if (input.tagName === "INPUT") {
-      input.value = "";
-    } else if (input.tagName === "SELECT") {
+    input.value = "";
+    if (input.tagName === "SELECT") {
       input.selectedIndex = 0;
     }
   });
 }
+
 // =====REMOVE INGREDIENT LINE======
 function removeIngredient(event) {
   event.preventDefault();
   const ingrInputs = event.target.closest(".ingredient-group");
   ingrInputs.remove();
   updateDeleteButtons();
+  updateIngredientLabels();
 }
 
 // =====UPDATE DEL BUTTON======
@@ -74,4 +92,39 @@ function updateDeleteButtons() {
   } else if (ingrInputs.length >= 2) {
     deleteButtons.forEach((button) => (button.disabled = false));
   }
+}
+
+// =====INPUTS VALIDATION======
+function validateTextInput(event) {
+  const input = event.target;
+
+  if (input.type === "text") {
+    input.value = input.value.replace(/[0-9]/g, "");
+  }
+}
+
+function validateNumInputs(event) {
+  if (
+    !/[0-9]/.test(event.key) &&
+    event.key !== "Backspace" &&
+    event.key !== "Delete" &&
+    event.key !== "ArrowLeft" &&
+    event.key !== "ArrowRight" &&
+    event.key !== "Tab" &&
+    event.key !== "." &&
+    event.key !== "Enter"
+  ) {
+    event.preventDefault();
+  }
+}
+
+// =====INGREDIENT LABEL NUMBER======
+
+function updateIngredientLabels() {
+  const ingredientGroups = document.querySelectorAll(".ingredient-group");
+
+  ingredientGroups.forEach((group, index) => {
+    const label = group.querySelector(".ingr-label");
+    label.textContent = "Ingredient " + (index + 1);
+  });
 }
